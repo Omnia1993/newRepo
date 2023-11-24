fc9c15af9315aad0c04e6ee2040336ccdc3a575f
Omnia <omniaeid266@gmail.com>
2023-11-24 15:54:30 -0600

    demo Ismail is pushed

---
+++ demoIsmail/src/main/java/com/example/demoIsmail/DemoIsmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoIsmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIsmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIsmailApplication.class, args);
+	}
+
+}
---
+++ demoIsmail/src/test/java/com/example/demoIsmail/DemoIsmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoIsmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIsmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
